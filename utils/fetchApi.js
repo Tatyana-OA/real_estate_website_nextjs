739bf73413f3bf2c78cdf83fa0eff77eb3e46587
Tatyana Asenova <tasenova@devrix.com>
2021-12-07 15:21:26 +0200

    Initial fetch and placement of properties

---
+++ utils/fetchApi.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+export const baseUrl = 'https://bayut.p.rapidapi.com';
+
+
+export const fetchApi = async (url) => {
+	const { data } = await axios.get((url), {
+		headers: {
+			'x-rapidapi-host': 'bayut.p.rapidapi.com',
+			'x-rapidapi-key': '0d2d77f1acmshbbd55caf3cd8b7fp148c31jsnfb83bec2a715'
+		  }
+	})
+	return data;
+}
