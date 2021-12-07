95bb21de1ea431cf7e7525711a3a23e3a40dc5da
Tatyana Asenova <tasenova@devrix.com>
2021-12-07 16:13:42 +0200

    Added footer

---
+++ components/Footer.js
@@ -0,0 +1,10 @@
+import { Box } from '@chakra-ui/react';
+
+
+const Footer = () => (
+	<Box textAlign="center" p="5" color="purple.600" borderTop="1px" borderColor="gray.700">
+		2021 Realtor Js. By Tanya Vass.
+	</Box>
+)
+
+export default Footer;
