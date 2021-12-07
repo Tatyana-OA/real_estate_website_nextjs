96b5056a6ebc418b66a3aa4e93aa3600869521a9
Tatyana Asenova <tasenova@devrix.com>
2021-12-07 17:45:13 +0200

    Search Filters created. Search funcitonality finished.

--- components/SearchFilters.js
+++ components/SearchFilters.js
@@ -4,10 +4,46 @@ import { useRouter } from 'next/router';
 import { MdCancel } from 'react-icons/md';
 import Image from 'next/image';
 
-const SearchFilters = () => {
-	return (
-	<h1>Search Filters Component</h1>
-	)
-}
+import { filterData, getFilterValues } from '../utils/filterData';
+import { baseUrl, fetchApi } from '../utils/fetchApi';
+import noresult from '../assets/images/no_results_found.png';
 
-export default SearchFilters;
+export default function SearchFilters() {
+  const [filters] = useState(filterData);
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  const searchProperties = (filterValues) => {
+    const path = router.pathname;
+    const { query } = router;
+
+    const values = getFilterValues(filterValues)
+
+    values.forEach((item) => {
+      if(item.value && filterValues?.[item.name]) {
+        query[item.name] = item.value
+      }
+    })
+
+    router.push({ pathname: path, query: query });
+  };
+
+
+
+  return (
+    <Flex bg='gray.100' p='4' justifyContent='center' flexWrap='wrap'>
+      {filters?.map((filter) => (
+        <Box key={filter.queryName}>
+          <Select onChange={(e) => searchProperties({ [filter.queryName]: e.target.value })} placeholder={filter.placeholder} w='fit-content' p='2' >
+            {filter?.items?.map((item) => (
+              <option value={item.value} key={item.value}>
+                {item.name}
+              </option>
+            ))}
+          </Select>
+        </Box>
+      ))}
+ 
+    </Flex>
+  );
+}
